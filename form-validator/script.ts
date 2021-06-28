42eaff3e20a177a179c6c5ab9e5f6c51c7b0280d
Koichi <koichi89592@gmail.com>
2021-06-28 10:45:36 +0000

    Set other checking method

--- form-validator/script.ts
+++ form-validator/script.ts
@@ -26,17 +26,37 @@ const checkRequired = (inputArray: HTMLInputElement[]) => {
   });
 };
 
+const checkLength = (input: HTMLInputElement, min: number, max: number) => {
+  if (input.value.length < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+  } else if (input.value.length > max) {
+    showError(input, `${getFieldName(input)} must be at less than ${max} characters`);
+  } else {
+    showSuccess(input);
+  }
+};
+
 const getFieldName = (input: HTMLInputElement) => {
   return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 };
 
-const isValidEmail = (email: string) => {
+const checkEmail = (input: HTMLInputElement) => {
   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(String(email).toLowerCase());
+
+  // return regex.test(String(email).toLowerCase());
+
+  if (regex.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email is not valid');
+  }
 };
 
 form.addEventListener('submit', (evt: Event) => {
   evt.preventDefault();
 
   checkRequired([username, email, password, password2]);
+  checkLength(username, 3, 15);
+  checkLength(password, 6, 25);
+  checkEmail(email);
 });
