90642a8c0cc1ffc29630da02fddd3b95d68b7a19
Koichi <koichi89592@gmail.com>
2021-07-18 18:34:04 +0900

    Add function of play, pause, and stop

---
+++ custom-video-player/script.ts
@@ -0,0 +1,51 @@
+const video = document.getElementById('video') as HTMLVideoElement;
+const playBtn = document.getElementById('play') as HTMLButtonElement;
+const stopBtn = document.getElementById('stop') as HTMLButtonElement;
+const progress = document.getElementById('progress') as HTMLProgressElement;
+const timestamp = document.getElementById('timestamp') as HTMLSpanElement;
+
+// Play & pause video
+const toggleVideoStatus = () => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+// Update play icon
+const updatePlayIcon = () => {
+  if (video.paused) {
+    playBtn.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    playBtn.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+};
+
+// update progress & timestamp
+const updateProgress = () => {
+  return true;
+};
+
+// Set video time to progress
+const setVideoProgress = () => {
+  return true;
+};
+
+// Stop video
+const stopVideo = () => {
+  video.currentTime = 0;
+  video.pause();
+};
+
+// Event listeners
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+playBtn.addEventListener('click', toggleVideoStatus);
+
+stopBtn.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
