7fae7a9df4aab1b3400cfa396386377d7c8d64af
Koichi <koichi89592@gmail.com>
2021-07-22 22:08:53 +0900

    Set fetch

--- exchange-rate/script.ts
+++ exchange-rate/script.ts
@@ -1,8 +1,36 @@
+const currencyEl_one = document.getElementById('currency-one') as HTMLSelectElement;
+const currencyEl_two = document.getElementById('currency-two') as HTMLSelectElement;
+const amountEl_one = document.getElementById('amount-one') as HTMLInputElement;
+const amountEl_two = document.getElementById('amount-two') as HTMLInputElement;
+
+const rateEl = document.getElementById('rate') as HTMLDivElement;
+const swap = document.getElementById('swap') as HTMLButtonElement;
+
+// Fetch exchange rates and update the DOM
 const calculate = () => {
-  fetch('items.json').then((res) => {
-    res.json().then((data) => {
-      console.log('data: ', data);
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(`https://api.exchangerate-api.com/v4/latest/${currency_one}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (Number.parseFloat(amountEl_one.value) * rate).toFixed(2);
     });
-  });
 };
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+});
+
 calculate();
