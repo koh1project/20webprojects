69b0a5f1e117786c56474ce33ffe979060b2abbf
Koichi <koichi89592@gmail.com>
2021-07-27 22:06:53 +0900

    Add basic project for Hangman

---
+++ hangman/script.js
@@ -0,0 +1,10 @@
+const wordEl = document.getElementById('word');
+const wrongLettersEl = document.getElementById('wrong-letters');
+const playAgainBtn = document.getElementById('play-button');
+const popup = document.getElementById('popup-container');
+const notification = document.getElementById('notification-container');
+const finalMessage = document.getElementById('final-message');
+
+const figureParts = document.querySelectorAll('.figure-part');
+
+const words = ['application', 'programming', 'interface', 'wizard'];
