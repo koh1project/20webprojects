389b3261304346bcd60365fac0eec4a66498f722
Koichi <koichi89592@gmail.com>
2021-08-03 05:48:15 +0000

    Add play and pause functions

--- music-player/script.js
+++ music-player/script.js
@@ -11,3 +11,43 @@ const cover = document.getElementById('cover');
 
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
+
+let songIndex = 2;
+
+const loadSong = (song) => {
+  title.innerText = song;
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+};
+
+const pauseSong = () => {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+
+  audio.pause();
+
+};
+
+const playSong = () => {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+  audio.play();
+};
+
+
+
+loadSong(songs[songIndex]);
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = musicContainer.classList.contains('play');
+
+  if (isPlaying) {
+    pauseSong();
+  }
+  else {
+    playSong();
+  }
+});
